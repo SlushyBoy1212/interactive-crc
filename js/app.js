606b2d48d0c98f6c781a66f9b71cbabda413c17d
antho <anthonyranalli5@gmail.com>
2025-10-27 18:20:01 -0700

    pages added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
+let page = document.getElementById('PageId')
 const searchParams = new URLSearchParams(window.location.search);
-if (searchParams.has("page")){
-    document.getElementById('PageId').innerText = searchParams.get('page');
+if (searchParams.has("page") && searchParams.get("page") !== "Contents") {
+    page.innerText = searchParams.get('page');
+    divshow = document.querySelectorAll('div.' + searchParams.get('page'));
+    divnone = document.querySelectorAll('div.none');
+    document.getElementById('home').classList.remove('hidden');
+    divshow.forEach(div => {
+        div.classList.remove('hidden');
+    })
+    divnone.forEach(div => {
+        div.style.display = 'none';
+    })
+} else {
+    page.innerText = "Contents";
 }
