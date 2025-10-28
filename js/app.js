475dc62f6740686c86f054e8853a2dfa67326868
antho <anthonyranalli5@gmail.com>
2025-10-27 18:40:28 -0700

    why are the "a" tags blue? - lcod

--- js/app.js
+++ js/app.js
@@ -3,14 +3,19 @@ const searchParams = new URLSearchParams(window.location.search);
 if (searchParams.has("page") && searchParams.get("page") !== "Contents") {
     page.innerText = searchParams.get('page');
     divshow = document.querySelectorAll('div.' + searchParams.get('page'));
-    divnone = document.querySelectorAll('div.none');
     document.getElementById('home').classList.remove('hidden');
+    divnone = document.querySelectorAll('div.none');
+    divnone.forEach(div => {
+        div.classList.add('hidden');
+    })
     divshow.forEach(div => {
         div.classList.remove('hidden');
     })
-    divnone.forEach(div => {
-        div.style.display = 'none';
-    })
+    if (searchParams.get("page") === "hidden") {
+        document.getElementById('id').classList.remove('hidden');
+    } else {
+        document.getElementById('id').innerText = "";
+    }
 } else {
     page.innerText = "Contents";
 }
