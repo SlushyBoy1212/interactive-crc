b286744fa631e5955b136b2bd94b6b41c52a97ac
antho <anthonyranalli5@gmail.com>
2025-10-27 17:39:29 -0700

    js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const page = document.getElementById('PageId');
+const pageId = document.getElementById('PageId');
 const searchParams = new URLSearchParams(window.location.search);
 if (searchParams.has("page")){
-    page.innerText = searchParams.page;
+    pageId.innerText = searchParams.page;
 }
